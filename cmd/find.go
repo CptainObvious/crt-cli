1e63dbe455736a156eb658b922f02afa1673e948
CptainObvious <obvious@420blaze.it>
2020-11-09 15:56:32 +0100

    Make domain args required and update Use

--- cmd/find.go
+++ cmd/find.go
@@ -1,18 +1,3 @@
-/*
-Copyright © 2020 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
@@ -25,15 +10,16 @@ import (
 	"github.com/cptainobvious/crt-cli/utils"
 )
 
-// findCmd represents the find command
 var (
 	blacklist []string
-	findCmd = &cobra.Command{
-		Use:   "find",
+	findCmd   = &cobra.Command{
+		Use:   "find domain",
 		Short: "Retrieve subdomains based on crt.sh",
+		Example: "crt-cli find domain.net",
 		Long: `This command retrieve subdomains of a domain based on ssl certificate transparency:
 It send a request to https://crt.sh/ with the query %.domainName to retrieve all subdomains certificate
 `,
+		Args: cobra.MinimumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			domainName, err := utils.GetDomainName(args[0])
 			if err != nil {
